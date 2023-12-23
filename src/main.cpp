1831a9ec789b0f83bb748caf3adc37a973f89f78
Brian Kloppenborg <brian@kloppenborg.net>
2023-12-23 14:34:17 -0700

    Customize cmake files for this project.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main (int argc, char *argv[])
   // Print out the project version. This variable is dynamically defined by
   // CMake using ${PROJECT_NAME} set in the main CMakeLists.txt and
   // template found in CMakeModules/version.h.mk.
-  std::cout << FB_VERSION << std::endl;
+  std::cout << QCT_VERSION << std::endl;
 
   return 0;
 }
